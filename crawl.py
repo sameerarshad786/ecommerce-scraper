a8ab9537a93444b11a3f082e2d149080a9932e8b
sameerarshad7771@gmail.com <sameerarshad7771@gmail.com>
2023-09-15 00:49:25 +0500

    running spider using CrawlerRunner

---
+++ crawl.py
@@ -0,0 +1,25 @@
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+from scrapy.utils.project import get_project_settings
+from twisted.internet import reactor
+
+from supermarket.spiders import DarazProductSpider, EbayProductsSpider
+
+
+def run_spiders():
+    configure_logging()
+
+    settings = get_project_settings()
+    settings.update({
+        "TWISTED_REACTOR": "twisted.internet.epollreactor.EPollReactor"
+    })
+    runner = CrawlerRunner(settings=settings)
+    runner.crawl(DarazProductSpider)
+    runner.crawl(EbayProductsSpider)
+
+    d = runner.join()
+    d.addBoth(lambda _: reactor.stop())
+
+    reactor.run()
+
+run_spiders()
